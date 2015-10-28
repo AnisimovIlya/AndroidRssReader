5d02a0667ec05482f2d4beff2b22c86397ac1313
anisimov <anisil@mail.ru>
2015-10-28 21:32:06 +0400

    improved main activity

--- News_02/app/src/main/java/com/test/ilya/news_02/MainActivity.java
+++ News_02/app/src/main/java/com/test/ilya/news_02/MainActivity.java
@@ -1,18 +1,23 @@
 package com.test.ilya.news_02;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         Button newsClickButton = (Button) findViewById(R.id.news_list_button);
         newsClickButton.setOnClickListener(
              new View.OnClickListener() {
